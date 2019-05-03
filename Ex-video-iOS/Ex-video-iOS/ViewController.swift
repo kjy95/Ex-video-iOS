5984abf86aa870cba313c0331912f408dbd22a70
김지영 <rmarkd32145@naver.com>
2019-05-03 23:50:57 +0900

    create project

---
+++ Ex-video-iOS/Ex-video-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ex-video-iOS
+//
+//  Created by 김지영 on 03/05/2019.
+//  Copyright © 2019 김지영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
