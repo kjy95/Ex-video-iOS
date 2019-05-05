b0fc495f008f82bf80d9c42e3e4f715861d4c7ee
김지영 <rmarkd32145@naver.com>
2019-05-06 01:31:03 +0900

    pause and play button

--- Ex-video-iOS/Ex-video-iOS/VideoLauncher.swift
+++ Ex-video-iOS/Ex-video-iOS/VideoLauncher.swift
@@ -26,18 +26,26 @@ class VideoPlayerView:UIView{
         return view
     }()
     
-    lazy var pauseButton: UIButton = {
+    lazy var pausePlayButton: UIButton = {
         let button = UIButton(type: .system)
         let image = UIImage(named: "pause")
         button.setImage(image, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.tintColor = .white
         button.addTarget(self, action: #selector(handlePause), for: .touchUpInside)
+        button.isHidden = true
         return button
     }()
+    var isPlaying = true
     @objc func handlePause(){
-        pauseButton.isHidden = true
-        player?.pause()
+        if isPlaying{
+            player?.pause()
+            pausePlayButton.setImage(UIImage(named: "play"), for: .normal)
+        }else{
+            player?.play()
+            pausePlayButton.setImage(UIImage(named: "pause"), for: .normal)
+        }
+        isPlaying = !isPlaying
     }
     override init(frame: CGRect){
         super.init(frame: frame)
@@ -56,11 +64,16 @@ class VideoPlayerView:UIView{
         
         setIndicatorViewOnCenterAndButton()
     }
+    
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        //player is ready and rendering frames
         if keyPath == "currentItem.loadedTimeRanges"{
             activityIndicatorView.stopAnimating()
             controlsContainerView.backgroundColor = UIColor.clear
+            pausePlayButton.isHidden = false
         }
+        
     }
     func setIndicatorViewOnCenterAndButton(){
         //activityIndicator
@@ -72,12 +85,12 @@ class VideoPlayerView:UIView{
         activityIndicatorView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         activityIndicatorView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         
-        controlsContainerView.addSubview(pauseButton)
+        controlsContainerView.addSubview(pausePlayButton)
         
-        pauseButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        pauseButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        pauseButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        pauseButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        pausePlayButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        pausePlayButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        pausePlayButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        pausePlayButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
